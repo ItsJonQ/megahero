53fbf09ba9685998db6e9d62690bd7d505774997
ItsJonQ <itsjonq@gmail.com>
2014-05-01 01:36:26 -0400

    Added watch, concat, and plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
 // Require Modules
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
 var cssmin = require('gulp-cssmin');
+var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 
 // Default
-gulp.task('default', ['scripts', 'styles']);
+gulp.task('default', ['scripts', 'styles', 'copy']);
 
 // CSS
 gulp.task('styles', function() {
+
     gulp.src('src/**/*.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
@@ -23,4 +26,27 @@ gulp.task('scripts', function() {
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist'));
+});
+
+// Copy
+gulp.task('copy', function() {
+    gulp.src('src/**/*.js')
+        .pipe(gulp.dest('./dist/'));
+
+    gulp.src('src/**/*.css')
+        .pipe(gulp.dest('./dist/'));
+});
+
+// Watch
+gulp.task('watch', function() {
+    gulp.src('src/**/*.js')
+        .pipe(watch())
+        .pipe(plumber())
+        .pipe(gulp.dest('./dist/'));
+
+    gulp.src('src/**/*.css')
+        .pipe(watch())
+        .pipe(plumber())
+        .pipe(gulp.dest('./dist/'));
+
 });
