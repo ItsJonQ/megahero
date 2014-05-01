9da9fe1ad82c5fea20a6f0f99605d78aef57b80b
ItsJonQ <itsjonq@gmail.com>
2014-05-01 01:02:14 -0400

    Added additional gulp packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 // Require Modules
 var gulp = require('gulp');
+var cssmin = require('gulp-cssmin');
 var browserify = require('gulp-browserify');
+var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 
 // Default
