d8b5d6b58a5d23c68775d952145a7f515f1ce0f3
ItsJonQ <itsjonq@gmail.com>
2014-05-01 01:12:37 -0400

    Reconfigured files with gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,26 @@
 // Require Modules
 var gulp = require('gulp');
-var cssmin = require('gulp-cssmin');
 var browserify = require('gulp-browserify');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 
 // Default
-gulp.task('default', function() {
+gulp.task('default', ['scripts', 'styles']);
 
+// CSS
+gulp.task('styles', function() {
+    gulp.src('src/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
+});
+
+// JS
+gulp.task('scripts', function() {
+    gulp.src('src/**/*.js')
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
 });
